0290905eca2ddb06530d7ec259be8ba7e52b71a5
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2021-12-10 16:08:26 +0100

    displaying category

--- back-office/admin.js
+++ back-office/admin.js
@@ -3,7 +3,7 @@ let submitForm = document.querySelector('#form-submit')
 let editBtn = document.querySelector('#edit-btn')
 let editIcon = document.querySelector('#editIcon')
 let deleteIcon = document.querySelector('#deleteIcon')
-
+let movieListNode = document.querySelector('#movie-list')
 // api
 let url = 'https://striveschool-api.herokuapp.com/api/movies'
 
@@ -56,7 +56,31 @@ const displayMovieList = async () => {
        let genreList = document.querySelector('#genre-list')
        genreList.addEventListener('change', async (event) => {
            let genreVal = event.target.value
+           let response = await fetch(`${url}/${genreVal}`, {
+               headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWZkMjRjZmY1ZjAwMTU5MGJkYjEiLCJpYXQiOjE2Mzg5NjkyOTgsImV4cCI6MTY0MDE3ODg5OH0.lsD_gQd4SZDzSyWedc_rbw92wUru5ae72RFzFCcksd0"
+               }
+           }) 
+           let movies = await response.json()
+           console.log(movies)
 
+           // displaying choosed movies
+           movies.forEach((movie) => {
+                let listItem = document.createElement('li')
+                listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items')
+                listItem.innerHTML = `
+                <div style="line-height: 5px;">
+                     <p class="font-weight-bold">${movie.name.charAt(0).toUpperCase() + movie.name.slice(1)}</p>
+                     <small class="text-muted font-weight-bold">${movie.category.charAt(0).toUpperCase() + movie.category.slice(1)}</small>
+                 </div>
+                 <div>
+                     <i data-id=${movie._id} id="editIcon" class="text-success bi bi-pencil-square mr-4" style="cursor: pointer;"></i>
+                     <i data-id=${movie._id} id="deleteIcon" class="text-danger bi bi-trash-fill" style="cursor: pointer;"></i>
+                 </div>
+                `
+                
+                movieListNode.appendChild(listItem)
+           });
            
        })
    } catch (error) {
